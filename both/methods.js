8c45d92e2fc778f0e0d7e700f1f3c1163499edc7
obloow <obloow.user@gmail.com>
2019-02-05 13:13:19 +0100

    ajout des checks sur les Meteor methods

--- both/methods.js
+++ both/methods.js
@@ -1,9 +1,15 @@
 import { Articles, Comments } from './collections';
 import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
 import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
 
 Meteor.methods({
     insertArticle(article) {
+        check(article, {
+            title: String,
+            content: String
+        });
+
         let articleDoc = {
             title: article.title,
             content: article.content,
@@ -14,12 +20,25 @@ Meteor.methods({
         return Articles.insert(articleDoc);
     },
     updateArticle(articleId, article) {
+        check(articleId, String);
+        check(article, {
+            title: String,
+            content: String
+        });
+
         Articles.update({ _id: articleId }, { $set: { title: article.title, content: article.content } });
     },
     removeArticle(articleId) {
+        check(articleId, String);
+
         Articles.remove({ _id: articleId });
     },
     insertComment(comment) {
+        check(comment, {
+            articleId: String,
+            content: String
+        })
+
         let commentDoc = {
             content: comment.content,
             articleId: comment.articleId,
