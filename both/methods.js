48ed6bc762f0c11bc4119a0270d8ea9359f949f2
obloow <obloow.user@gmail.com>
2019-02-05 17:38:42 +0100

    ajout du controle utilisateur connecté dans les Meteor methods

--- both/methods.js
+++ both/methods.js
@@ -7,6 +7,10 @@ Meteor.methods({
     insertArticle(article) {
         articleUpsertSchema.validate(article);
 
+        if(!this.userId) {
+            throw new Meteor.Error('not-connected');
+        }
+
         let articleDoc = {
             title: article.title,
             content: article.content,
@@ -19,15 +23,27 @@ Meteor.methods({
     updateArticle(article) {
         articleUpsertSchema.validate(article);
 
+        if(!this.userId) {
+            throw new Meteor.Error('not-connected');
+        }
+
         Articles.update({ _id: article.id }, { $set: { title: article.title, content: article.content } });
     },
     removeArticle(articleId) {
         check(articleId, String);
 
+        if(!this.userId) {
+            throw new Meteor.Error('not-connected');
+        }
+
         Articles.remove({ _id: articleId });
     },
     insertComment(comment) {
         commentInsertSchema.validate(comment);
+
+        if(!this.userId) {
+            throw new Meteor.Error('not-connected');
+        }
 
         let commentDoc = {
             content: comment.content,
